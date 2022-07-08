63a76b549ea4fa689bf0d3fe238b76173380d848
ChalakaNiroda <108949451+ChalakaNiroda@users.noreply.github.com>
2022-07-09 01:06:12 +0530

    done

---
+++ src/main/java/EAD2/CW/EAD2/CW/Ead2CwApplication.java
@@ -0,0 +1,13 @@
+package EAD2.CW.EAD2.CW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ead2CwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ead2CwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EAD2/CW/EAD2/CW/Ead2CwApplicationTests.java
@@ -0,0 +1,13 @@
+package EAD2.CW.EAD2.CW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ead2CwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
